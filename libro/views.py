d84e2d21f11aa9a68efe4ff8fd5a84c8ee2e63a6
HenryGP14 <perezh010@gmail.com>
2021-08-28 13:09:49 -0500

    Configurando setting y creando la clase ApiView libro

--- libro/views.py
+++ libro/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class Libro(APIView):
+
+    def get(self, request, format=None):
+            if request.method == "GET":
+                try:
+                   
+                    return Response({"consulta": ""})
+                except Exception as ex:
+                    return Response({"mensaje": "Ups, no se encuentra registrado el libro...."})
+
+
